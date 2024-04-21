177a29b745b6012bec2e747daa808aeb9f0685b5
msygt <167672995+msygt@users.noreply.github.com>
2024-04-21 00:52:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,117 @@
-import altair as alt
+import streamlit as st
 import numpy as np
 import pandas as pd
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+df=pd.read_csv('trend_yorum.csv.zip',on_bad_lines="skip",delimiter=";")
+
+
+def temizle(sutun):
+    stopwords = ['fakat', 'lakin', 'ancak', 'acaba', 'ama', 'aslında', 'az', 'bazı', 'belki', 'biri', 'birkaç',
+                 'birşey', 'biz', 'bu', 'çok', 'çünkü', 'da', 'daha', 'de', 'defa', 'diye', 'eğer', 'en', 'gibi', 'hem',
+                 'hep', 'hepsi', 'her', 'hiç', 'için', 'ile', 'ise', 'kez', 'ki', 'kim', 'mı', 'mu', 'mü', 'nasıl',
+                 'ne', 'neden', 'nerde', 'nerede', 'nereye', 'niçin', 'niye', 'o', 'sanki', 'şey', 'siz', 'şu', 'tüm',
+                 've', 'veya', 'ya', 'yani']
+    semboller = string.punctuation
+    sutun = sutun.lower()
+    for sembol in semboller:
+        sutun = sutun.replace(sembol, " ")
+
+    for stopword in stopwords:
+        s = " " + stopword + " "
+        sutun = sutun.replace(s, " ")
+
+    sutun = sutun.replace("  ", " ")
+
+    return sutun
+
+
+df['Metin'] = df['Metin'].apply(temizle)
+
+cv=CountVectorizer(max_features=250)
+X=cv.fit_transform(df['Metin']).toarray()
+y=df['Durum']
+
+x_train,x_test,y_train,y_test=train_test_split(X,y,train_size=0.75,random_state=42)
+
+y=st.text_area("Yorum metni giriniz: ")
+btn=st.button("Yorumu kategorilendir")
+if btn:
+    rf = RandomForestClassifier()
+    model = rf.fit(x_train, y_train)
+    skor=model.score(x_test, y_test)
+    st.balloons()
+    tahmin = cv.transform(np.array([y])).toarray()
+    kat = {
+        1: "Olumlu",
+        0: "Olumsuz",
+        2: "Nötr"
+    }
+    sonuc = model.predict(tahmin)
+    s=kat.get(sonuc[0])
+    st.subheader(s)
+    st.write("Model Skoru: ",skor)
+
+kod="""
 import streamlit as st
+import numpy as np
+import pandas as pd
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
+df=pd.read_csv('trend_yorum.csv.zip',on_bad_lines="skip",delimiter=";")
+
+
+def temizle(sutun):
+    stopwords = ['fakat', 'lakin', 'ancak', 'acaba', 'ama', 'aslında', 'az', 'bazı', 'belki', 'biri', 'birkaç',
+                 'birşey', 'biz', 'bu', 'çok', 'çünkü', 'da', 'daha', 'de', 'defa', 'diye', 'eğer', 'en', 'gibi', 'hem',
+                 'hep', 'hepsi', 'her', 'hiç', 'için', 'ile', 'ise', 'kez', 'ki', 'kim', 'mı', 'mu', 'mü', 'nasıl',
+                 'ne', 'neden', 'nerde', 'nerede', 'nereye', 'niçin', 'niye', 'o', 'sanki', 'şey', 'siz', 'şu', 'tüm',
+                 've', 'veya', 'ya', 'yani']
+    semboller = string.punctuation
+    sutun = sutun.lower()
+    for sembol in semboller:
+        sutun = sutun.replace(sembol, " ")
+
+    for stopword in stopwords:
+        s = " " + stopword + " "
+        sutun = sutun.replace(s, " ")
+
+    sutun = sutun.replace("  ", " ")
+
+    return sutun
+
+
+df['Metin'] = df['Metin'].apply(temizle)
+
+cv=CountVectorizer(max_features=250)
+X=cv.fit_transform(df['Metin']).toarray()
+y=df['Durum']
+
+x_train,x_test,y_train,y_test=train_test_split(X,y,train_size=0.75,random_state=42)
+
+y=st.text_area("Yorum metni giriniz: ")
+btn=st.button("Yorumu kategorilendir")
+if btn:
+    rf = RandomForestClassifier()
+    model = rf.fit(x_train, y_train)
+    skor=model.score(x_test, y_test)
+    st.balloons()
+    tahmin = cv.transform(np.array([y])).toarray()
+    kat = {
+        1: "Olumlu",
+        0: "Olumsuz",
+        2: "Nötr"
+    }
+    sonuc = model.predict(tahmin)
+    s=kat.get(sonuc[0])
+    st.subheader(s)
+    st.write("Model Skoru: ",skor)
 """
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+st.code(kod,language="python")
