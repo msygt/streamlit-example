03b6fcb79c419fd3ea94889adc71529db5490593
hizlipython <167673092+hizlipython@users.noreply.github.com>
2024-04-21 11:44:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ if btn:
     st.subheader(s)
     st.write("Model Skoru:",skor)
 
-    c.execute("INSERT INTO yorumlar VALUES(?,?,?)",(y,s,zaman))
+    c.execute("INSERT INTO testler VALUES(?,?,?)",(y,s,zaman))
     conn.commit()
 
 
